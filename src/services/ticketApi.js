2756cd46b2303658902eb3ddbec5f96ab50a1824
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-08-05 13:47:44 -0300

    feat: tickets services

---
+++ src/services/ticketApi.js
@@ -0,0 +1,32 @@
+import api from './api';
+
+export async function getTicketTypes(token) {
+  const response = await api.get('/tickets/types', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  
+  return response.data;
+}
+
+export async function createTicket(ticketTypeId, token) {
+  const body = { ticketTypeId };
+  const response = await api.post('/tickets', body, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  
+  return response.data;
+}
+
+export async function getTicket(token) {
+  const response = await api.get('/tickets', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  
+  return response.data;
+}
