b1f0fdbdd16bebfbfd56efce09e90b5c5209d96c
isaias <figueiredo.isaias@gmail.com>
2023-08-07 16:43:55 -0300

    feat: payment

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,23 +1,66 @@
 import styled from "styled-components";
+import React, { useState } from "react";
+import { AuthContext } from "../../../contexts/enrolmentContext";
+
+
+
 
 export default function Payment() {
+
+  const { enroll, setEnroll } = React.useContext(AuthContext)
+
+
+  const [ticket,setTicket]= useState('')
+  const [choosedticket,setChoosedticket]=useState(false)
+  const [ticketValue,setTicketValue] =useState('')
+  const [checkEnrollment] = useState(false)
+  const[review,setReview]=useState(false)
+
+  function presentialTicket(){
+    setChoosedticket(true)
+    setTicket('presential')
+    setTicketValue("250")
+  }
+
+  function onlineTicket(){
+    setChoosedticket(true)
+    setTicket('online')
+    setTicketValue('100')
+
+  }
+  function checkTicket(){
+    setReview(true)
+  }
+
+
   return (
+
+
+
     <Containner>
-      <h1>ingresso e pagamento</h1>
-      <p>escolha o tipo de ingresso:</p>
-      <div>
-        <button className="presential">
-          presencial
+      {checkEnrollment ? <div2> <p>você precisa se inscrever primeiro
+      </p> </div2> : <>
+        <h1>ingresso e pagamento</h1>
+        <p>escolha o tipo de ingresso:</p>
+        <div>
+          <button className="presential" onClick={()=>{presentialTicket()}}>
+            presencial
+          </button>
+          <button className="online" onClick={()=>{onlineTicket()}}>
+            online
+          </button>
+
+        </div>
+        {choosedticket? <>
+         <p>fechado, o total ficou em R${ticketValue}. Agora é só confirmar:</p>
+        <button onClick={()=>{checkTicket()}}>
+          reservar ingresso
         </button>
-        <button className="online">
-          online
-        </button>
+        </>
+        :''}
         
-      </div>
-      <p>fechado, o total ficou em R$100. Agora é só confirmar:</p>
-      <button>
-        reservar ingresso
-      </button>
+      </>}
+
     </Containner>
 
 
@@ -64,7 +107,17 @@ button{
   height: 30px;
   border-radius: 5px;
 }
-
+div2{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  p{
+     color: black;
+  font-size: 20px;
+  }
+}
 
 display: flex;
 
